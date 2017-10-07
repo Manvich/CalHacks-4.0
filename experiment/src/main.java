ea10efcbcff6d4aa265d9bbc552604ef056cf801
Jimin Park <jp0524@berkeley.edu>
2017-10-07 15:43:50 -0700

    some mods to experiment

--- experiment/src/main.java
+++ experiment/src/main.java
@@ -1,3 +1,4 @@
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -5,8 +6,9 @@ import java.util.List;
  * Created by Lin on 10/7/17.
  */
 public class main {
+
     public static void main (String[] arg){
-        List<Object> object = new ArrayList<Object>();
+        ArrayList<ArrayList> object = new ArrayList<ArrayList>();
         for (int i = 0; i < 81; i++){
             ArrayList<Integer> store = new ArrayList<Integer>();
             for (int j = 1; j < 10; j++){
@@ -18,6 +20,16 @@ public class main {
 
         int[][] board = new int[9][9];
         while (true){
+            if (noMorePossibleValues(object)) {
+                break;
+            } else {
+
+            }
+
+
+            /*if(noMorePossibleValues(object)
+            *   break
+            *   else*/
 
         }
 
@@ -26,4 +38,32 @@ public class main {
     int[] generator (int x){
 
     }
+
+    public static ArrayList createPossibleIndice(int[][] board, ArrayList<ArrayList> object) {
+        ArrayList answer = new ArrayList();
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                if (board[i][j] > 0) {
+
+                }
+            }
+        }
+    }
+
+
+    /*returns true if all the tiles have 1 or less possible values */
+    public static boolean noMorePossibleValues(ArrayList<ArrayList> object) {
+        boolean answer = true;
+        for (int i = 0; i < 81; i ++) {
+            ArrayList current = object.get(i);
+            if (current.size() <= 1) {
+                answer = false;
+                return answer;
+            }
+        }
+        return answer;
+    }
+
+
+
 }
