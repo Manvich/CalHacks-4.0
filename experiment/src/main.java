441fc34c2a75515e4b741fa2e51d84f2360c69a9
Jimin Park <jp0524@berkeley.edu>
2017-10-07 17:42:24 -0700

    fixed some unchecked errors

--- experiment/src/main.java
+++ experiment/src/main.java
@@ -8,16 +8,18 @@ import java.util.Collections;
  */
 public class main {
 
-    public static void main (String[] arg){
+    public static void main(String[] arg) {
+
         ArrayList<ArrayList> object = new ArrayList<ArrayList>();
-        for (int i = 0; i < 81; i++){
+        for (int i = 0; i < 81; i++) {
+
             ArrayList<Integer> store = new ArrayList<Integer>();
-            for (int j = 1; j < 10; j++){
+            for (int j = 1; j < 10; j++) {
                 store.add(i);
             }
             object.add(store);
         }
-        
+
 
         int[][] board = new int[9][9];
         /*while (true){
@@ -32,18 +34,12 @@ public class main {
             *   break
             *   else*/
 
-        }
-
-
-    }
-    int[] generator (int x){
-
     }
 
 
     /*Returns an ArrayList containing the indices of all empty spaces on the board*/
-    public static ArrayList createPossibleIndices(int[][] board, ArrayList<ArrayList> object) {
-        ArrayList<int[]> answer = new ArrayList();
+    public static ArrayList<int[]> createPossibleIndices(int[][] board, ArrayList<ArrayList> object) {
+        ArrayList<int[]> answer = new ArrayList<int[]>();
         for (int i = 0; i < 9; i++) {
             for (int j = 0; j < 9; j++) {
                 if (board[i][j] == 0) {
@@ -57,23 +53,23 @@ public class main {
 
     /*from the list of possible indices, choose the ones with the maximum possibilities and return them as
     * a new arraylist*/
-    public static ArrayList chooseBestIndices(ArrayList<int[]> possibleIndices, ArrayList<ArrayList> object) {
+    public static ArrayList<int[]> chooseBestIndices(ArrayList<int[]> possibleIndices, ArrayList<ArrayList> object) {
 
-        ArrayList answer = new ArrayList();
+        ArrayList<int[]> answer = new ArrayList<int[]>();
         Collections.copy(possibleIndices, answer);
         int max = 0;
 
-        for (int i = 0; i < possibleIndices.size(); i ++) {
+        for (int i = 0; i < possibleIndices.size(); i++) {
             int[] current = possibleIndices.get(i);
-            ArrayList possibleNums = object.get(9*current[0]+current[1]);
-            if (max < d.size()) {
+            ArrayList possibleNums = object.get(9 * current[0] + current[1]);
+            if (max < possibleNums.size()) {
                 max = possibleNums.size();
             }
         }
 
-        for (int i = 0; i < possibleIndices.size(); i ++) {
+        for (int i = 0; i < possibleIndices.size(); i++) {
             int[] current = possibleIndices.get(i);
-            ArrayList possibleNums = object.get(9*current[0]+current[1]);
+            ArrayList possibleNums = object.get(9 * current[0] + current[1]);
             if (possibleNums.size() == max) {
                 answer.add(current);
             }
@@ -85,8 +81,8 @@ public class main {
     /*returns true if all the tiles have 1 or less possible values */
     public static boolean noMorePossibleValues(ArrayList<ArrayList> object) {
         boolean answer = true;
-        for (int i = 0; i < 81; i ++) {
-            ArrayList current = object.get(i);
+        for (int i = 0; i < 81; i++) {
+            ArrayList<Integer> current = object.get(i);
             if (current.size() <= 1) {
                 answer = false;
                 return answer;
@@ -94,7 +90,5 @@ public class main {
         }
         return answer;
     }
-
-
-
 }
+
