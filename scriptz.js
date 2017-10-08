81ef854c2894f64dd0310f36f183ebce6374669a
Manvich <_jellyfish_@berkeley.edu>
2017-10-08 02:21:55 -0700

    working puzzle generator

--- scriptz.js
+++ scriptz.js
@@ -35,16 +35,43 @@ function check(matrix, textBoxIDs) {
 }
 
 function generateMatrix() {
-	/*var matrix = new Array(9);
+	var matrix = new Array(9);
+	var possibleValues;
+	var partitions = [""];
+	var tempPartitions = [];
 	for (var x = 0; x < 9; x++) {
 		matrix[x] = new Array(9);
 		for (var y = 0; y < 9; y++) {
-			var b = 1 == parseInt(Math.random()*2, 10);
-			matrix[x][y] = b ? parseInt(Math.random()*9, 10) + 1 : -1;
+			possibleValues = [1,2,3,4,5,6,7,8,9];
+			for (var x2 = 0; x2 < x; x2++) {
+				var temp = possibleValues.indexOf(matrix[x2][y]);
+				if (temp >= 0)
+					possibleValues.splice(temp, 1);
+			}
+			for (var mx = x - x%3; mx < x; mx++) {
+				for (var my = y - y%3; my < y+3-y%3; my++) {
+					var temp = possibleValues.indexOf(matrix[mx][my]);
+					if (temp >= 0)
+						possibleValues.splice(temp, 1);
+				}
+			}
+			for (var z = 0; z < partitions.length; z++) {
+				for (var zz = 0; zz < possibleValues.length; zz++) {
+					if (partitions[z].indexOf(possibleValues[zz]) == -1) {
+						tempPartitions.push(partitions[z] + possibleValues[zz]);
+					}
+				}
+			}
+			partitions = tempPartitions;
+			tempPartitions = [];
 		}
+		var row = partitions[parseInt(Math.random()*partitions.length,10)];
+		for (var y = 0; y < 9; y++) {
+			matrix[x][y] = parseInt(row.charAt(y), 10);
+		}
+		partitions = [""];
 	}
-	return matrix;*/
-	return createBoard();
+	return matrix;
 }
 
 function displayMatrix(matrix) {
@@ -76,52 +103,4 @@ function displayMatrix(matrix) {
 		}
 	}
 	return textBoxIDs;
-}
-
-function createBoard() {
-	var board = new Array(9);
-	var possibleValues = new Array(9);
-	for (var x = 0; x < 9; x++) {
-		board[x] = new Array(9);
-		possibleValues[x] = new Array(9);
-		for (var y = 0; y < 9; y++) {
-			possibleValues[x][y] = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-			board[x][y] = -1;
-		}
-	}
-	while(true) {
-		var greatestPotential = 0;
-		var indices = [];
-		for (var x = 0; x < 9; x++) {
-			for(var y = 0; y < 9; y++) {
-				if (possibleValues[x][y].length > greatestPotential) {
-					greatestPotential = possibleValues[x][y].length;
-					indices = [x*10+y];
-				} else if (possibleValues[x][y].length == greatestPotential) {
-					indices.push(x*10+y);
-				}
-			}
-		}
-		if (greatestPotential == 1)
-			return board;
-		var index = indices[parseInt(Math.random()*indices.length,10)];
-		var tmpx = parseInt(index/10,10), tmpy = index%10;
-		var value = possibleValues[parseInt(tmpx)][tmpy][parseInt(Math.random()*possibleValues[tmpx][tmpy].length,10)];
-		board[tmpx][tmpy] = value;
-		possibleValues[tmpx][tmpy] = [value];
-		for (var x = 0; x < 9; x++) {
-			var columnIndex = possibleValues[tmpx][x].indexOf(value);
-			var rowIndex = possibleValues[x][tmpy].indexOf(value);
-			var modX = parseInt(tmpx/3, 10) * 3 + x%3;
-			var modY = parseInt(tmpy/3, 10) * 3 + parseInt(x/3, 10);
-			var modIndex = possibleValues[modX][modY].indexOf(value);
-			if (x != tmpy && columnIndex >= 0)
-				possibleValues[tmpx][x].splice(columnIndex, 1);
-			if (x != tmpx && rowIndex >= 0)
-				possibleValues[x][tmpy].splice(rowIndex, 1);
-			if ((modX != tmpx || modY != tmpy) && modIndex >= 0) {
-				possibleValues[modX][modY].splice(modIndex,1);
-			}
-		}
-	}
 }
